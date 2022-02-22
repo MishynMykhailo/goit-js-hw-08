d9db320d5e67c4fabb02803c15e76facf1a5b573
MishynMykhailo <misham18092001@gmail.com>
2022-02-22 23:23:30 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,17 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryEl = document.querySelector('.gallery')
+galleryEl.innerHTML = createGalleryMarkup(galleryItems)
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+function createGalleryMarkup(items){
+    return items.map(({ preview, original, description })=>{
+        return `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`
+    }).join("");
+}
