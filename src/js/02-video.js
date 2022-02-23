91b18169580eacf51997ee0ce3a74bd0c3136b1d
MishynMykhailo <misham18092001@gmail.com>
2022-02-23 23:28:15 +0200

    2 task - complited

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+const _ = require('lodash');
+
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const previousVideoProgress = localStorage.getItem(LOCALSTORAGE_KEY);
+
+if (previousVideoProgress){
+    player.setCurrentTime(Number(previousVideoProgress))
+}
+
+
+player.on('timeupdate', _.throttle(onTimeUpdate, 1000));
+function onTimeUpdate (e){
+localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(e.seconds))
+}
 
