5c873b815d5dbd27895e2a630ae49e6338de153e
MishynMykhailo <misham18092001@gmail.com>
2022-02-23 23:56:04 +0200

    3 task update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+const _ = require('lodash');
+const LOCALSTORAGE_KEY = "feedback-form-state";
 
+const formEl = document.querySelector(".feedback-form")
+
+formEl.addEventListener('input',_.throttle(onFormInput))
+formEl.addEventListener('submit',_.throttle(onFormSumbit))
+
+checkLocalStorage();
+function onFormInput(e){
+localStorage.setItem(LOCALSTORAGE_KEY,JSON.stringify(createStatusForm))
+};
+
+function createStatusForm(){
+    return {
+        email: formEl.elements.email.value,
+        message: formEl.elements.message.value,
+      };
+    
+}
+
+function onFormSumbit(e){
+    e.preventDefault();
+    console.log(createStatusForm());
+    formEl.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+};
+    
+
+function checkLocalStorage(){
+    let currentFieldStatus;
+    try {
+      currentFieldStatus = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    } catch (error) {
+      
+    }
+}
